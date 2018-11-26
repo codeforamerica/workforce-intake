d13b68920a5fef85fd2f99eee9d7d2fe1b84b0dc
Ben Golder <bgolder@codeforamerica.org>
2018-11-26 15:14:39 -0800

    Add CfA styleguide
    Co-authored-by: Leo Mancini <leo@codeforamerica.org>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/", to: "workforce_applications#new"
   resources :workforce_applications
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Cfa::Styleguide::Engine => "/cfa"
 end
