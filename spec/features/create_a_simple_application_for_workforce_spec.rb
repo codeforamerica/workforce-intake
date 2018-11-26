7cb2f9f726fb4b51390f4293be889e98aa4a8366
Ben Golder <bgolder@codeforamerica.org>
2018-11-26 15:56:54 -0800

    Use CfAFormBuilder for form

--- spec/features/create_a_simple_application_for_workforce_spec.rb
+++ spec/features/create_a_simple_application_for_workforce_spec.rb
@@ -4,12 +4,12 @@ RSpec.feature "create a simple application for workforce services" do
   scenario "with a minimal situation" do
     visit "/"
 
-    fill_in "Email", with: "user@example.com"
-    fill_in "Phone number", with: "1234567890"
-    fill_in "Household size", with: "2"
-    fill_in "Earned income", with: "2000"
+    fill_in "What is your email?", with: "user@example.com"
+    fill_in "What is your phone number?", with: "1234567890"
+    fill_in "How many people do you live with and share money with?", with: "2"
+    fill_in "How much money do you make each month?", with: "2000"
 
-    click_on "Create Workforce application"
+    click_on "Continue"
 
     expect(page.text).to include "Workforce application was successfully created."
   end
